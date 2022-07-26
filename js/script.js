1f76916fdcd2ff3c17d944260c68c6bffe55b429
Felipe Chiesa <felipechiesa05@gmail.com>
2022-07-26 19:07:35 -0300

    Primera Entrega PF JS

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-function Producto(nombre, precio, stock) {
+function Producto(id, nombre, precio, stock) {
+    this.id = id;
     this.nombre = nombre;
     this.precio = precio;
     this.stock = stock;
 }
 
-const producto1 = new Producto('Hoodie One', 50, 20)
-const producto2 = new Producto('Hoodie Two', 20, 5)
-const producto3 = new Producto('Hoodie Three', 35, 10)
-const producto4 = new Producto('Hoodie Four', 60, 25)
-const producto5 = new Producto('Hoodie Five', 45, 15)
+const producto1 = new Producto('green hoodie one', 'Green Hoodie One', 50, 20)
+const producto2 = new Producto('green hoodie two', 'Green Hoodie Two', 20, 5)
+const producto3 = new Producto('black hoodie three', 'Black Hoodie Three', 35, 10)
+const producto4 = new Producto('blue hoodie four', 'Blue Hoodie Four', 60, 25)
+const producto5 = new Producto('red hoodie five', 'Red Hoodie Five', 45, 15)
 
 let listaProductos = [producto1, producto2, producto3, producto4, producto5]
 
@@ -16,16 +17,11 @@ let preciototal = 0
 
 let cupon = "CODER"
 
-let nombresProductos = []
+let nombresProductos = listaProductos.map((producto) => producto.nombre)
 
-function listarproductos(){
-    for(const producto of listaProductos) {
-        nombresProductos.push(producto.nombre)
-    }
-    }
-    listarproductos()
+let idProductos = listaProductos.map((producto) => producto.id)
 
-let cantidadcompras = prompt("Cuantos productos desea comprar? \n- " + nombresProductos.join('\n- '))
+let cantidadcompras = prompt("Cuantos productos distintos desea comprar? \n- " + nombresProductos.join('\n- '))
 
 if(cantidadcompras > 5) {
     alert("Solo disponemos de 5 productos diferentes")
@@ -53,21 +49,34 @@ function calculostock(cantidad, producto) {
 for(let i = 0 ; i < cantidadcompras ; i++) {
 
     let buzo = prompt("Que producto/s desea comprar? \n- " + nombresProductos.join('\n- '))
+
+    const resultado = listaProductos.filter((producto) => producto.id.includes(buzo.toLowerCase()))
+
+    const nombresResultado = resultado.map((producto) => producto.nombre)
+
+    let busqueda = ''
+
+    if(resultado.length > 1 || (resultado.length === 1 && buzo.toLowerCase() !== resultado[0].id)) {
+    
+    busqueda = prompt('Estos productos coinciden con tu busqueda: \n- ' + (nombresResultado.join('\n- ')))
+
+    }
+
     let cantidad = prompt("Cuantos productos desea comprar?")
 
-    if(buzo === producto1.nombre) {
+    if((buzo.toLowerCase() === producto1.nombre.toLowerCase()) || (busqueda.toLowerCase() === producto1.nombre.toLowerCase())) {
         calculostock(cantidad, producto1)
     }
-    else if(buzo === producto2.nombre) {
+    else if((buzo.toLowerCase() === producto2.nombre.toLowerCase()) || (busqueda.toLowerCase() === producto2.nombre.toLowerCase())) {
         calculostock(cantidad, producto2)
     }
-    else if(buzo === producto3.nombre) {
+    else if((buzo.toLowerCase() === producto3.nombre.toLowerCase()) || (busqueda.toLowerCase() === producto3.nombre.toLowerCase())) {
         calculostock(cantidad, producto3)
     }
-    else if(buzo === producto4.nombre) {
+    else if((buzo.toLowerCase() === producto4.nombre.toLowerCase()) || (busqueda.toLowerCase() === producto4.nombre.toLowerCase())) {
         calculostock(cantidad, producto4)
     }
-    else if(buzo === producto5.nombre) {
+    else if((buzo.toLowerCase() === producto5.nombre.toLowerCase()) || (busqueda.toLowerCase() === producto5.nombre.toLowerCase())) {
         calculostock(cantidad, producto5)
     }
     else {
@@ -81,3 +90,5 @@ let ahorro = prompt("Introduzca un cupon de descuento")
 if(ahorro === "CODER") {
 alert("El precio total es de $" + descuento(preciototal))
 }
+
+alert('Nro de ticket: ' + (Math.random()))
