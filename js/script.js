f8161a413e33b9217a45cd5aed20bb2f8eb152bb
Felipe Chiesa <felipechiesa05@gmail.com>
2022-08-09 20:29:54 -0300

    carrito agregado

--- js/script.js
+++ js/script.js
@@ -1,34 +1,55 @@
-function Producto(id, nombre, precio, stock, img) {
-    this.id = id;
-    this.nombre = nombre;
-    this.precio = precio;
-    this.stock = stock;
-    this.img = img;
-}
+let preciototal = 0
 
-const producto1 = new Producto('pink hoodie one', 'Pink Hoodie One', 50, 20, "./multimedia/imagenes/hoodie 2.jpg")
-const producto2 = new Producto('green hoodie two', 'Green Hoodie Two', 20, 5, "./multimedia/imagenes/hoodie 3.jpeg")
-const producto3 = new Producto('orange hoodie three', 'Orange Hoodie Three', 35, 10, "./multimedia/imagenes/151-fbbfb36c77118f8ca516387096049002-480-0.jpg")
-const producto4 = new Producto('white hoodie four', 'White Hoodie Four', 60, 25, "./multimedia/imagenes/buzo-311-eb31f9107f1bdb8fef16433181181071-480-0.jpg")
-const producto5 = new Producto('white hoodie five', 'White Hoodie Five', 45, 15, "./multimedia/imagenes/buzo-1011-7917b6b641fb4db03616433168665966-640-0.jpg")
-
-let listaProductos = [producto1, producto2, producto3, producto4, producto5]
+let cupon = "CODER"
 
 const tienda = document.getElementById('tienda')
+
+const carritoCompra = document.getElementById('carrito')
+
+let carrito = []
+
+let compraFinalizada = false
+
+function Producto(id, name, price, stock, image) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.stock = stock;
+    this.image = image;
+}
+
+let listaProductos = [
+    new Producto(1, 'Pink Hoodie One', 50, 20, "./multimedia/imagenes/hoodie 2.jpg"), 
+    new Producto(2, 'Green Hoodie Two', 20, 5, "./multimedia/imagenes/hoodie 3.jpeg"), 
+    new Producto(3, 'Orange Hoodie Three', 35, 10, "./multimedia/imagenes/151-fbbfb36c77118f8ca516387096049002-480-0.jpg"), 
+    new Producto(4, 'White Hoodie Four', 60, 25, "./multimedia/imagenes/buzo-311-eb31f9107f1bdb8fef16433181181071-480-0.jpg"), 
+    new Producto(5, 'White Hoodie Five', 45, 15, "./multimedia/imagenes/buzo-1011-7917b6b641fb4db03616433168665966-640-0.jpg")
+]
+
+let nombresProductos = listaProductos.map((product) => product.nombre)
+
+let idProductos = listaProductos.map((product) => product.id)
+
+const carritoDeCompras = () => {
+    let iconCarrito = document.createElement('div')
+    iconCarrito.innerHTML = 
+
+    carrito.appendChild(carrito)
+}
 
 const visualizarProductos = () => {
     tienda.innerHTML = ''
 
-    listaProductos.forEach((producto) => {
-        let cards = document.createElement("div")
+    listaProductos.forEach((product, index) => {
+        let cards = document.createElement('div')
         cards.innerHTML = 
     `<main class="mt-5 mb-5 shop-container">
         <section class="shop d-flex justify-content-around">
             <div class="card animation" style="width: 18rem;">
-                <img src="${producto.img}" class="card-img-top" alt="Hoodies">
+                <img src="${product.image}" class="card-img-top" alt="Hoodies">
                     <div class="card-body">
-                        <h5 class="card-title">${producto.nombre}</h5>
-                        <a href="#" class="btn btn-dark">$${producto.precio}</a>
+                        <h5 class="card-title">${product.name}</h5>
+                        <button href="#" class="btn btn-dark" onClick="agregarAlCarrito(${index})">$${product.price}</button>
                     </div>
             </div>
         </section>
@@ -40,17 +61,13 @@ const visualizarProductos = () => {
 )   
 }
 
-const menor = document.getElementById('menor')
-const mayor = document.getElementById('mayor')
-
 mayor.onclick = () =>{
-    console.log(menor)
-    listaProductos = sortArray(listaProductos, 'precio')
+    listaProductos = sortArray(listaProductos, 'price')
     visualizarProductos()
 }
 
 menor.onclick = () =>{
-    listaProductos = sortArray(listaProductos, 'precio', 'desc')
+    listaProductos = sortArray(listaProductos, 'price', 'desc')
     visualizarProductos()
 }
 
@@ -64,87 +81,120 @@ const sortArray = (array, column, direction = 'asc') => {
     return array
 }
 
-const arrayOrdenado = sortArray(listaProductos, 'precio', 'desc')
-console.log(arrayOrdenado)
+const arrayOrdenado = sortArray(listaProductos, 'price', 'desc')
 
-let preciototal = 0
+const agregarAlCarrito = (index) => {
+    const indiceProducto = carrito.findIndex(({id}) => {
+        return id === listaProductos[index].id
+    });
 
-let cupon = "CODER"
+    if (indiceProducto > -1) {
+        carrito[indiceProducto].cantidad += 1
+        actualizarStorage(carrito);
+        imprimirCarrito();
+    } 
+    else {
+        const agregarProducto = listaProductos[index];
+        agregarProducto.cantidad = 1;
+        carrito.push(agregarProducto);
+        actualizarStorage(carrito);
+        imprimirCarrito();
+    }
+}
 
-let nombresProductos = listaProductos.map((producto) => producto.nombre)
+const finalizarCompra = () => {
+    localStorage.removeItem('carrito')
+    compraFinalizada = true
+    imprimirCarrito()
+}
 
-let idProductos = listaProductos.map((producto) => producto.id)
 
-// let cantidadcompras = prompt("Cuantos productos distintos desea comprar? \n- " + nombresProductos.join('\n- '))
 
-// if(cantidadcompras > 5) {
-//     alert("Solo disponemos de 5 productos diferentes")
-// }
+const imprimirCarrito = () => {
+    let total = 0;
+    carritoCompra.innerHTML = "";
+    carrito = localStorage.getItem("carrito") ? JSON.parse(localStorage.getItem("carrito")) : [];
 
-// function calculoprecio(cantidad, precio) {
-
-//     preciototal += cantidad * precio
-// }
-
-// function descuento(calculoprecio) {
-//     return calculoprecio * 0.90
-// }
-
-// function calculostock(cantidad, producto) {
-//     if(cantidad <= producto.stock) {
-//         calculoprecio(cantidad, producto.precio)
-//         alert("El precio es de: $" + (cantidad * producto.precio))
-//     }
-//     else {
-//         alert("No disponemos de ese stock")
-//     }
-// }
-
-// for(let i = 0 ; i < cantidadcompras ; i++) {
-
-//     let buzo = prompt("Que producto/s desea comprar? \n- " + nombresProductos.join('\n- '))
-
-//     const resultado = listaProductosGeneral.filter((producto) => producto.id.includes(buzo.toLowerCase()))
-
-//     const nombresResultado = resultado.map((producto) => producto.nombre)
-
-//     let busqueda = ''
-
-//     if(resultado.length > 1 || (resultado.length === 1 && buzo.toLowerCase() !== resultado[0].id)) {
+    const footer = document.getElementById("modal-footer");
+    const footerContent =  `<button type="button" class="btn btn-danger" onClick="vaciarCarrito()">Vaciar Carrito</button>`;
+    const footerContentLeft = carrito.length ?  `<button type="button" class="btn btn-dark" onClick=finalizarCompra()>Comprar</button>` : '';
     
-//     busqueda = prompt('Estos productos coinciden con tu busqueda: \n- ' + (nombresResultado.join('\n- ')))
+    
+    if (carrito.length > 0) {
+        carrito.forEach((product, index) => {
+            total = total + product.price * product.cantidad;
+            const listaCarrito = document.createElement("div");
+            listaCarrito.className = "text-center";
+            listaCarrito.innerHTML = 
+            `<div class="row">
+                <table>
+                    <td>
+                        <div class=" d-flex align-items-center">
+                            <div class="px-3 pb-3"><a><img src="${product.image}" alt="${product.name}" width="48"></a></div>
+                            <div class="d-sm-flex"></div>
+                            <div class="align-middle px-sm-5"><h2 class="lead my-1">${product.name}</h2></div>
+                            <div class="align-middle">Cant. ${product.cantidad}</div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="d-flex align-items-center">
+                            <div class="px-4"><h2 class="lead my-1">$${product.price}</h2></div>
+                            <div class="">
+                                <span>
+                                    <div class="p-2"> <a onClick="removeProduct(${index})"><i class="fa-solid fa-trash-can"></i></a></div>
+                                </span>
+                            </div>
+                        </div>
+                    </td>
+                </table>
+            </div>`
 
-//     }
+            carritoCompra.appendChild(listaCarrito);
+        });
 
-//     let cantidad = prompt("Cuantos productos desea comprar?")
+const imprimirTotal = document.createElement("div");
+        imprimirTotal.innerHTML = 
+        `<div class="p-5"> TOTAL $${total.toFixed(2)}</div>`;
 
-//     if((buzo.toLowerCase() === producto1.nombre.toLowerCase()) || (busqueda.toLowerCase() === producto1.nombre.toLowerCase())) {
-//         calculostock(cantidad, producto1)
-//     }
-//     else if((buzo.toLowerCase() === producto2.nombre.toLowerCase()) || (busqueda.toLowerCase() === producto2.nombre.toLowerCase())) {
-//         calculostock(cantidad, producto2)
-//     }
-//     else if((buzo.toLowerCase() === producto3.nombre.toLowerCase()) || (busqueda.toLowerCase() === producto3.nombre.toLowerCase())) {
-//         calculostock(cantidad, producto3)
-//     }
-//     else if((buzo.toLowerCase() === producto4.nombre.toLowerCase()) || (busqueda.toLowerCase() === producto4.nombre.toLowerCase())) {
-//         calculostock(cantidad, producto4)
-//     }
-//     else if((buzo.toLowerCase() === producto5.nombre.toLowerCase()) || (busqueda.toLowerCase() === producto5.nombre.toLowerCase())) {
-//         calculostock(cantidad, producto5)
-//     }
-//     else {
-//         alert("No disponemos de ese producto")
-//     }
-// }
-// alert("El precio total es de $" + preciototal)
+        carritoCompra.appendChild(imprimirTotal);
+    } else if(!compraFinalizada) {
+        carritoCompra.classList.remove("carrito")
+        carritoCompra.innerHTML =
+        `<div class="alert alert-light" role="alert">
+        El carrito está vacío
+        </div>`;
+    } else {
+        carritoCompra.classList.remove("carrito")
+        carritoCompra.innerHTML =
+        `<div class="alert alert-light" role="alert">
+        La compra se ha realizado con exito!
+        </div>`;
+    }
 
-// let ahorro = prompt("Introduzca un cupon de descuento")
 
-// if(ahorro === "CODER") {
-// alert("El precio total es de $" + descuento(preciototal))
-// }
+    footer.innerHTML = `${footerContentLeft} ${footerContent}`;
+};
 
-// alert('Nro de ticket: ' + (Math.random()))
+
+const removeProduct = (index) => {
+    carrito.splice (index, 1);
+    actualizarStorage(carrito);
+    imprimirCarrito();
+}
+
+const actualizarStorage = (carrito) => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+const calcularCantProductos = () => {
+    return carrito.reduce((total, elemento) => total + elemento.cantidad, 0);
+}
+
+vaciarCarrito = () => {
+    localStorage.removeItem("carrito");
+    imprimirCarrito()
+};
 
 visualizarProductos()
+imprimirCarrito(); 
+
